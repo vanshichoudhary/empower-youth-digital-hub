452ab07a2793a0719074f02c1eed348d1e1875cb
cvanshika113 <cvanshika113@gmail.com>
2025-04-25 12:03:51 +0000

    7th

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,11 @@
+import React from 'react';
 
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Home from "./pages/Home";
-import IDCardPage from "./pages/IDCard";
-import HighlightsPage from "./pages/Highlights";
-import PartnersPage from "./pages/Partners";
-import NotFound from "./pages/NotFound";
-import AIHumanGuidance from "./pages/AIHumanGuidance";
+const AIHumanGuidance: React.FC = () => {
+  return (
+    <div>
+      {/* Your content here */}
+    </div>
+  );
+};
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Header />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/id-card" element={<IDCardPage />} />
-            <Route path="/highlights" element={<HighlightsPage />} />
-            <Route path="/partners" element={<PartnersPage />} />
-            <Route path="/ai-human-guidance" element={<AIHumanGuidance />} />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default AIHumanGuidance;
---
+++ src/pages/AIHumanGuidance.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EmptyPage: React.FC = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default EmptyPage;
